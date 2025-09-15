971d845de25e7d9775b6d20c684328eff0e45485
Rodrigo Medina <rodrigomedina@MacBook-Air-de-Rodrigo.local>
2025-09-15 17:30:47 -0300

    feat: Configurar GitHub Actions y Pages para despliegue automático

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repoName = 'cvalinstante';
+
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/',
   css: {
     postcss: './postcss.config.js',
   },
